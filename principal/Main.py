c95236f3b3701e444375e27a24f0e3ef740a9f0c
vitorDaynno <vitordaynno@gmail.com>
2017-07-12 22:06:01 -0300

    Inserção no banco funcionando

--- principal/Main.py
+++ principal/Main.py
@@ -1,11 +1,19 @@
 #Vitor Daynno 16/06/2017
+# -*- coding: utf-8 -*-
 
+from pymongo import MongoClient
 from trello import TrelloApi
 from Config import Config
+
+cliente = MongoClient('localhost',27017)
+banco = cliente['Pontuacao']
+tabela = banco['tarefas']
 
 config = Config()
 trello = TrelloApi(config.get_Api_Key(),'none')  #API Key
 trello.set_token(config.get_Token()) #Token
+id = 0
+
 for quadro in config.get_Quadros():
     board = trello.boards.get(str(quadro))
     print "\nQuadro: " + board["name"]
@@ -16,8 +24,21 @@ for quadro in config.get_Quadros():
         print '   Card: '+card["name"]
         card_Detalhes = trello.cards.get_action(card["id"])
         try:
-           if str(card_Detalhes[0]["data"]["text"]) == "u":
-                mudar = trello.cards.update_idList(card["id"],lista[len(lista)-1]["id"])
+           if card_Detalhes[0]["data"]["text"][:11] == "Dificuldade":
+                print card_Detalhes[0]["data"]["text"][13:]
+                if card_Detalhes[0]["data"]["text"][13:].encode('utf-8') == "Fácil":
+                    tabela.insert_one({"_id":id,"nome": card["name"], "pontos": 1})
+
+                elif card_Detalhes[0]["data"]["text"][13:].encode('utf-8') == "Média":
+                    tabela.insert_one({"_id":id,"nome" : card["name"], "pontos":  2 }) 
+
+                elif card_Detalhes[0]["data"]["text"][13:].encode('utf-8') == "Difícil":
+                    tabela.insert_one({"_id":id,"nome" : card["name"], "pontos":  3 }) 
+                #mudar = trello.cards.update_idList(card["id"],lista[len(lista)-1]["id"])
                 print '     Computado com sucesso'
+           elif card_Detalhes[0]["data"]["text"][:6] == "Pontos":
+                print int(card_Detalhes[0]["data"]["text"][8:])
+                tabela.insert_one({"_id":id,"nome" : card["name"], "pontos": int(card_Detalhes[0]["data"]["text"][8:])})
+           id = id + 1
         except KeyError:
              print '     Falha ao computar'
